42e8208757df5db27c24bad9a9b4a3b3ed19c195
Søren Debois <soren.debois@gmail.com>
2016-12-26 16:21:19 +0100

    Upgrade to elm 0.18

--- examples/FancyButton.elm
+++ examples/FancyButton.elm
@@ -1,14 +1,14 @@
 module FancyButton
-  exposing
-    ( Msg
-    , Model
-    , Handler
-    , model
-    , update
-    , view
-    , onMouseEnter
-    , onMouseLeave
-    )
+    exposing
+        ( Msg
+        , Model
+        , Handler
+        , model
+        , update
+        , view
+        , onMouseEnter
+        , onMouseLeave
+        )
 
 import Html exposing (..)
 import Html.Attributes exposing (style)
@@ -17,42 +17,49 @@ import Json.Decode as Json
 
 
 -- MODEL
+
+
 {-| Model is opaque as it contains internal state.
- -}
+-}
 type Model
-  = Model
-      { hovered : Bool
-      , hoverCount : Int
-      }
+    = Model
+        { hovered : Bool
+        , hoverCount : Int
+        }
+
 
 {-| Initialize the model
- -}
+-}
 model : Model
 model =
-  Model
-    { hovered = False
-    , hoverCount = 0
-    }
+    Model
+        { hovered = False
+        , hoverCount = 0
+        }
+
 
 
 -- Options
 
+
 {-| An event handler for FancyButton
- -}
+-}
 type Handler msg
-  = Handler String (Json.Decoder msg)
+    = Handler String (Json.Decoder msg)
+
 
 {-|
- -}
+-}
 onMouseEnter : msg -> Handler msg
 onMouseEnter msg =
-  Handler "mouseenter" (Json.succeed msg)
+    Handler "mouseenter" (Json.succeed msg)
+
 
 {-|
- -}
+-}
 onMouseLeave : msg -> Handler msg
 onMouseLeave msg =
-  Handler "mouseleave" (Json.succeed msg)
+    Handler "mouseleave" (Json.succeed msg)
 
 
 
@@ -60,29 +67,30 @@ onMouseLeave msg =
 
 
 type Msg msg
-  = Hover
-  | Leave
-    {- Batch a list of messages for `Dispatch`
-     -}
-  | Batch (List msg)
+    = Hover
+    | Leave {- Batch a list of messages for `Dispatch` -}
+    | Batch (List msg)
 
 
 update : Msg msg -> Model -> ( Model, Cmd msg )
 update msg (Model model) =
-  case msg of
-    {- Forward all the messages produced by event-handlers
-    with multiple decoders attached to them
-    -}
-    Batch msg' ->
-      Model model ! [ Dispatch.forward msg' ]
+    case msg of
+        {- Forward all the messages produced by event-handlers
+           with multiple decoders attached to them
+        -}
+        Batch msg_ ->
+            Model model ! [ Dispatch.forward msg_ ]
 
-    Hover ->
-      Model { model | hovered = True
-            , hoverCount = model.hoverCount + 1 }
-            ! []
+        Hover ->
+            Model
+                { model
+                    | hovered = True
+                    , hoverCount = model.hoverCount + 1
+                }
+                ! []
 
-    Leave ->
-      Model { model | hovered = False } ! []
+        Leave ->
+            Model { model | hovered = False } ! []
 
 
 
@@ -91,39 +99,37 @@ update msg (Model model) =
 
 view : (Msg msg -> msg) -> Model -> List (Handler msg) -> List (Attribute msg) -> List (Html msg) -> Html msg
 view lift (Model model) handlers attributes content =
-  let
-    {- We want to perform internal actions on these events
-     -}
-    defaultListeners =
-      [ onMouseEnter (lift Hover)
-      , onMouseLeave (lift Leave)
-      ]
+    let
+        {- We want to perform internal actions on these events -}
+        defaultListeners =
+            [ onMouseEnter (lift Hover)
+            , onMouseLeave (lift Leave)
+            ]
 
-    {- Setup the Dispatch configuration using user-provided event-handlers
-    plus our own internal event-handlers
-    -}
-    config =
-      List.foldl
-        (\handler acc ->
-          case handler of
-            Handler evt d ->
-              Dispatch.add evt Nothing d acc
-        )
-        (Dispatch.setMsg (Batch >> lift) Dispatch.defaultConfig)
-        (handlers ++ defaultListeners)
-
-  in
-    button
-      ([ normal
-       , if model.hovered then
-          hovered model.hoverCount
-         else
-          style []
-       ]
-        ++ attributes
-        ++ (Dispatch.toAttributes config)
-      )
-      content
+        {- Setup the Dispatch configuration using user-provided event-handlers
+           plus our own internal event-handlers
+        -}
+        config =
+            List.foldl
+                (\handler acc ->
+                    case handler of
+                        Handler evt d ->
+                            Dispatch.add evt Nothing d acc
+                )
+                (Dispatch.setMsg (Batch >> lift) Dispatch.defaultConfig)
+                (handlers ++ defaultListeners)
+    in
+        button
+            ([ normal
+             , if model.hovered then
+                hovered model.hoverCount
+               else
+                style []
+             ]
+                ++ attributes
+                ++ (Dispatch.toAttributes config)
+            )
+            content
 
 
 
@@ -132,36 +138,36 @@ view lift (Model model) handlers attributes content =
 
 normal : Attribute a
 normal =
-  style
-    [ ( "display", "inline-block" )
-    , ( "margin", "0 10px 0 0" )
-    , ( "padding", "15px 15px" )
-    , ( "font-size", "16px" )
-    , ( "line-height", "1.8" )
-    , ( "appearance", "none" )
-    , ( "box-shadow", "none" )
-    , ( "border-radius", "0" )
-    ]
+    style
+        [ ( "display", "inline-block" )
+        , ( "margin", "0 10px 0 0" )
+        , ( "padding", "15px 15px" )
+        , ( "font-size", "16px" )
+        , ( "line-height", "1.8" )
+        , ( "appearance", "none" )
+        , ( "box-shadow", "none" )
+        , ( "border-radius", "0" )
+        ]
 
 
 hovered : Int -> Attribute a
 hovered count =
-  let
-    colors =
-      [ ("background-color", "#b6d8e4")
-      , ("background-color", "#e4a8a8")
-      , ("background-color", "#f6a8e4")
-      ]
+    let
+        colors =
+            [ ( "background-color", "#b6d8e4" )
+            , ( "background-color", "#e4a8a8" )
+            , ( "background-color", "#f6a8e4" )
+            ]
 
-    nrColors =
-        List.length colors
+        nrColors =
+            List.length colors
 
-    color =
-      List.drop (count % nrColors) colors
-        |> List.head
-        |> Maybe.withDefault ("background-color", "#b6d8e4")
-  in
-    style
-      [ color
-      , ( "outline", "none" )
-      ]
+        color =
+            List.drop (count % nrColors) colors
+                |> List.head
+                |> Maybe.withDefault ( "background-color", "#b6d8e4" )
+    in
+        style
+            [ color
+            , ( "outline", "none" )
+            ]
--- src/Dispatch.elm
+++ src/Dispatch.elm
@@ -1,10 +1,18 @@
-module Dispatch exposing
-  ( Config, toAttributes, add, setMsg, setDecoder, getDecoder, defaultConfig
-  , clear
-  , on, onWithOptions
-  , update
-  , forward
-  )
+module Dispatch
+    exposing
+        ( Config
+        , toAttributes
+        , add
+        , setMsg
+        , setDecoder
+        , getDecoder
+        , defaultConfig
+        , clear
+        , on
+        , onWithOptions
+        , update
+        , forward
+        )
 
 {-| Dispatch multiple messages in response to a single `Html.Event`.
 
@@ -42,30 +50,31 @@ import Task
 
 -- CONFIG
 
+
 {-| Dispatch configuration type
- -}
-type Config msg =
-  Config
-    { decoders : List (String, (Decoder msg, Maybe Html.Events.Options))
-    , lift : Maybe (Decoder (List msg) -> Decoder msg)
-    }
+-}
+type Config msg
+    = Config
+        { decoders : List ( String, ( Decoder msg, Maybe Html.Events.Options ) )
+        , lift : Maybe (Decoder (List msg) -> Decoder msg)
+        }
 
 
 {-| Empty configuration
- -}
+-}
 defaultConfig : Config msg
 defaultConfig =
-  Config
-    { decoders = []
-    , lift = Nothing
-    }
+    Config
+        { decoders = []
+        , lift = Nothing
+        }
 
 
 {-| Tell Dispatch how to convert a list of decoders into a decoder for a single message.
 -}
 setDecoder : (Decoder (List msg) -> Decoder msg) -> Config msg -> Config msg
 setDecoder f (Config config) =
-  Config { config | lift = Just f }
+    Config { config | lift = Just f }
 
 
 {-| Tell Dispatch how to convert a list of messages into a single message. Alternative
@@ -73,49 +82,51 @@ to `setDecoder`.
 -}
 setMsg : (List msg -> msg) -> Config msg -> Config msg
 setMsg =
-  Json.map >> setDecoder
+    Json.map >> setDecoder
 
 
 {-| Get the Dispatch message constructor
 -}
 getDecoder : Config msg -> Maybe (Decoder (List msg) -> Decoder msg)
 getDecoder (Config config) =
-  config.lift
+    config.lift
 
 
 {-| Add an event-handler to the current configuration
- -}
+-}
 add : String -> Maybe Html.Events.Options -> Decoder msg -> Config msg -> Config msg
 add event options decoder (Config config) =
-  Config
-    { config | decoders = (event, (decoder, options)) :: config.decoders }
+    Config
+        { config | decoders = ( event, ( decoder, options ) ) :: config.decoders }
 
 
 {-| Clear event handlers in current configuration
 -}
 clear : Config msg -> Config msg
 clear (Config config) =
-  Config
-    { config | decoders = [] }
+    Config
+        { config | decoders = [] }
 
 
 {-| Returns a list of `Html.Attribute` containing handlers that
 dispatch multiple decoders on a single `Html.Event`
- -}
+-}
 toAttributes : Config msg -> List (Html.Attribute msg)
 toAttributes (Config config) =
-  case config.lift of
-    Just f ->
-      List.map (onMany f) (group config.decoders)
-    Nothing ->
-      List.map onSingle config.decoders
+    case config.lift of
+        Just f ->
+            List.map (onMany f) (group config.decoders)
+
+        Nothing ->
+            List.map onSingle config.decoders
 
 
 {-| Promote `msg` to `Cmd msg`
 -}
 cmd : msg -> Cmd msg
 cmd msg =
-  Task.perform (always msg) (always msg) (Task.succeed msg)
+    Task.perform (always msg) (Task.succeed msg)
+
 
 
 -- UPDATE
@@ -123,9 +134,9 @@ cmd msg =
 
 {-| Maps messages to commands
 -}
-forward : (List msg) -> Cmd msg
+forward : List msg -> Cmd msg
 forward messages =
-  List.map cmd messages |> Cmd.batch
+    List.map cmd messages |> Cmd.batch
 
 
 {-| Map the second element of a tuple
@@ -134,103 +145,112 @@ forward messages =
 -}
 map2nd : (b -> c) -> ( a, b ) -> ( a, c )
 map2nd f ( x, y ) =
-  ( x, f y )
+    ( x, f y )
 
 
-update1 : (m -> model -> (model, d)) -> m -> ( model, List d ) -> ( model, List d )
+update1 : (m -> model -> ( model, d )) -> m -> ( model, List d ) -> ( model, List d )
 update1 update cmd ( m, gs ) =
-  update cmd m
-    |> map2nd (flip (::) gs)
+    update cmd m
+        |> map2nd (flip (::) gs)
 
 
 {-| Runs the given `update` on all the messages and
 returns the updated model including batching of
 any commands returned by running the `update`.
 -}
-update : (msg -> model -> (model, Cmd obs)) -> (List msg) -> model -> (model, Cmd obs)
+update : (msg -> model -> ( model, Cmd obs )) -> List msg -> model -> ( model, Cmd obs )
 update update msg model =
-  List.foldl (update1 update) (model, []) msg
-    |> map2nd Cmd.batch
+    List.foldl (update1 update) ( model, [] ) msg
+        |> map2nd Cmd.batch
+
 
 
 -- VIEW
-
-
-decode : List (Decoder m) -> Json.Value -> Result c (List m)
-decode decoders v0 =
-  List.filterMap (flip Json.decodeValue v0 >> Result.toMaybe) decoders
-    |> Result.Ok
 
 
 {-| Applies given decoders to the same initial value
    and return the applied results as a list
 -}
 flatten : List (Decoder m) -> Decoder (List m)
-flatten =
-  decode >> Json.customDecoder Json.value
+flatten decoders =
+    Json.value
+        |> Json.map
+            (\value ->
+                List.filterMap
+                    (\decoder ->
+                        Json.decodeValue decoder value |> Result.toMaybe
+                    )
+                    decoders
+            )
 
 
 {-| Dispatch multiple decoders for a single event.
- -}
-on
-  : String
-  -> (List msg -> msg)
-  -> List (Decoder msg)
-  -> Html.Attribute msg
+-}
+on :
+    String
+    -> (List msg -> msg)
+    -> List (Decoder msg)
+    -> Html.Attribute msg
 on event lift =
-  onWithOptions event lift Html.Events.defaultOptions
+    onWithOptions event lift Html.Events.defaultOptions
 
 
 {-| Dispatch multiple decoders for a single event.
 Options apply to the whole event.
- -}
-onWithOptions
-  : String
-  -> (List msg -> msg)
-  -> Html.Events.Options
-  -> List (Decoder msg)
-  -> Html.Attribute msg
+-}
+onWithOptions :
+    String
+    -> (List msg -> msg)
+    -> Html.Events.Options
+    -> List (Decoder msg)
+    -> Html.Attribute msg
 onWithOptions event lift options decoders =
-  flatten decoders
-    |> Json.map lift
-    |> Html.Events.onWithOptions event options
+    flatten decoders
+        |> Json.map lift
+        |> Html.Events.onWithOptions event options
 
 
 {-| Run multiple decoders on a single Html Event with
 the given options
 -}
-onMany
-  : (Decoder (List m) -> Decoder m)
-  -> ( String, List ( Decoder m, Maybe Html.Events.Options ) )
-  -> Html.Attribute m
+onMany :
+    (Decoder (List m) -> Decoder m)
+    -> ( String, List ( Decoder m, Maybe Html.Events.Options ) )
+    -> Html.Attribute m
 onMany lift decoders =
-  case decoders of
-    -- Install direct handler for singleton case
-    (event, [ decoder ])  ->
-      onSingle (event, decoder)
+    case decoders of
+        -- Install direct handler for singleton case
+        ( event, [ decoder ] ) ->
+            onSingle ( event, decoder )
 
-    (event, decoders) ->
-      flatten (List.map fst decoders)
-        |> lift
-        |> Html.Events.onWithOptions event (pickOptions decoders)
+        ( event, decoders ) ->
+            flatten (List.map Tuple.first decoders)
+                |> lift
+                |> Html.Events.onWithOptions event (pickOptions decoders)
 
 
 pickOptions : List ( a, Maybe Html.Events.Options ) -> Html.Events.Options
 pickOptions decoders =
-  case decoders of
-    (_, Just options) :: _ -> options
-    _ :: rest -> pickOptions rest
-    [] -> Html.Events.defaultOptions
+    case decoders of
+        ( _, Just options ) :: _ ->
+            options
+
+        _ :: rest ->
+            pickOptions rest
+
+        [] ->
+            Html.Events.defaultOptions
 
 
-onSingle
-    : (String, (Decoder m, Maybe Html.Events.Options ))
+onSingle :
+    ( String, ( Decoder m, Maybe Html.Events.Options ) )
     -> Html.Attribute m
-onSingle (event, (decoder, option)) =
-  Html.Events.onWithOptions
-    event
-      (Maybe.withDefault Html.Events.defaultOptions option)
-      decoder
+onSingle ( event, ( decoder, option ) ) =
+    Html.Events.onWithOptions
+        event
+        (Maybe.withDefault Html.Events.defaultOptions option)
+        decoder
+
 
 
 -- UTILITIES
@@ -241,45 +261,45 @@ onSingle (event, (decoder, option)) =
 -}
 group : List ( a, b ) -> List ( a, List b )
 group =
-  group' []
+    group_ []
 
 
 split :
-  a
-  -> List b
-  -> List ( a, b )
-  -> List ( a, b )
-  -> ( List b, List ( a, b ) )
+    a
+    -> List b
+    -> List ( a, b )
+    -> List ( a, b )
+    -> ( List b, List ( a, b ) )
 split k0 same differ xs =
-  case xs of
-    [] ->
-      ( same, differ )
+    case xs of
+        [] ->
+            ( same, differ )
 
-    (( k, v ) as x) :: xs ->
-      if k == k0 then
-        split k0 (v :: same) differ xs
-      else
-        split k0 same (x :: differ) xs
+        (( k, v ) as x) :: xs ->
+            if k == k0 then
+                split k0 (v :: same) differ xs
+            else
+                split k0 same (x :: differ) xs
 
 
-group' : List ( a, List b ) -> List ( a, b ) -> List ( a, List b )
-group' acc items =
-  case items of
-    [] ->
-      acc
+group_ : List ( a, List b ) -> List ( a, b ) -> List ( a, List b )
+group_ acc items =
+    case items of
+        [] ->
+            acc
 
-    [ ( k, v ) ] ->
-      ( k, [ v ] ) :: acc
+        [ ( k, v ) ] ->
+            ( k, [ v ] ) :: acc
 
-    [ ( k1, v1 ), ( k2, v2 ) ] ->
-      if k1 == k2 then
-        ( k1, [ v2, v1 ] ) :: acc
-      else
-        ( k2, [ v2 ] ) :: ( k1, [ v1 ] ) :: acc
+        [ ( k1, v1 ), ( k2, v2 ) ] ->
+            if k1 == k2 then
+                ( k1, [ v2, v1 ] ) :: acc
+            else
+                ( k2, [ v2 ] ) :: ( k1, [ v1 ] ) :: acc
 
-    ( k, v ) :: xs ->
-      let
-        ( same, different ) =
-          split k [ v ] [] xs
-      in
-        group' (( k, same ) :: acc) different
+        ( k, v ) :: xs ->
+            let
+                ( same, different ) =
+                    split k [ v ] [] xs
+            in
+                group_ (( k, same ) :: acc) different
